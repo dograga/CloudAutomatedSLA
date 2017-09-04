22eeb387401b5489bf7e32e760f33c4e89988472
Gaurav Dogra <gaurav.dogra@gmail.com>
2017-09-04 14:57:44 +0800

    Update monthlysla.py

--- monthlysla.py
+++ monthlysla.py
@@ -8,7 +8,7 @@ import datetime
 from calendar import monthrange
 
 
-logfile="/root/sla/log/monthlyreport_check.log"
+logfile="" ## Log file location
 log=logit.logit(logfile)
 
 def getMonthdays(year,month):
